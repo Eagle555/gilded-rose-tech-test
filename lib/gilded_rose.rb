5c705475247d6ca2f60f1c32744b7c65804de7d9
Rob <rob.eagleheart@gmail.com>
2021-11-24 16:52:45 +0000

    Refactor tests, code and text

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,6 @@
-require_relative "item"
+# frozen_string_literal: true
+
+require_relative 'item'
 
 class GildedRose
   def initialize(items)
@@ -7,7 +9,7 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      item.name.include?("Aged") || item.name.include?("Backstage") ? quality_increase(item) : quality_decrese(item)
+      item.name.include?('Aged') || item.name.include?('Backstage') ? quality_increase(item) : quality_decrese(item)
       time_control(item)
     end
   end
@@ -15,16 +17,20 @@ class GildedRose
   private
 
   def quality_increase(item)
-    item.name.include?("Aged") || item.sell_in > 10 ? item.quality += 1 : item.sell_in > 5 ? item.quality += 2 : (item.quality += 3 if item.sell_in > 0)
+    if item.name.include?('Aged') || item.sell_in > 10
+      item.quality += 1
+    else
+      item.sell_in > 5 ? item.quality += 2 : (item.quality += 3 if item.sell_in.positive?)
+    end
     item.quality = 50 if item.quality > 50
   end
 
   def quality_decrese(item)
-    item.name.include?("Conjured") ? item.quality -= 2 : item.quality -= 1 unless item.name.include?("Sulfuras")
-    item.quality = 0 if item.name.include?("Backstage") && item.sell_in < 0 || item.quality < 0
+    item.quality -= item.name.include?('Conjured') ? 2 : 1 unless item.name.include?('Sulfuras')
+    item.quality = 0 if item.name.include?('Backstage') && item.sell_in.negative? || item.quality.negative?
   end
 
   def time_control(item)
-    item.sell_in -= 1 unless item.name.include?("Sulfuras")
+    item.sell_in -= 1 unless item.name.include?('Sulfuras')
   end
 end
--- spec/legacy_test_spec.rb
+++ spec/legacy_test_spec.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 require 'gilded_rose'
 
-puts "Legacy test"
-puts "---------------------------"
+puts 'Legacy test'
+puts '---------------------------'
 items = [
-  Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
-  Item.new(name="Aged Brie", sell_in=2, quality=0),
-  Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-  Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-  Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
-  Item.new(name="Conjured Mana Cake", sell_in=3, quality=6),
+  Item.new(name = '+5 Dexterity Vest', sell_in = 10, quality = 20),
+  Item.new(name = 'Aged Brie', sell_in = 2, quality = 0),
+  Item.new(name = 'Elixir of the Mongoose', sell_in = 5, quality = 7),
+  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = 0, quality = 80),
+  Item.new(name = 'Sulfuras, Hand of Ragnaros', sell_in = -1, quality = 80),
+  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 15, quality = 20),
+  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 10, quality = 49),
+  Item.new(name = 'Backstage passes to a TAFKAL80ETC concert', sell_in = 5, quality = 49),
+  Item.new(name = 'Conjured Mana Cake', sell_in = 3, quality = 6)
 ]
 
-days = 6
+days = 3
 
 gilded_rose = GildedRose.new items
 (0...days).each do |day|
   puts "-------- day #{day} --------"
-  puts "name, sellIn, quality"
+  puts 'name, sellIn, quality'
   items.each do |item|
     puts item
   end
-  puts ""
+  puts ''
   gilded_rose.update_quality
 end
-puts "---------------------------"
-puts "test end"
+puts '---------------------------'
+puts 'test end'
